7c9125eaf7ef8d9cdc0146de8cb2a61e77399ece
Richard Holzner <richard.holzner@gmail.com>
2026-04-06 18:57:49 +0200

    Add collected heli callsigns from scanner

---
+++ server/data/helicopters.js
@@ -0,0 +1,47 @@
+export const HELICOPTERS = [
+  // --- Christoph (DRF / ADAC) ---
+  { callsign: 'CHX1', name: 'Christoph 1', operator: 'ADAC', base: 'München-Harlaching' },
+  { callsign: 'CHX15', name: 'Christoph 15', operator: 'DRF', base: 'Straubing' },
+  { callsign: 'CHX17', name: 'Christoph 17', operator: 'DRF', base: 'Kempten-Durach' },
+  { callsign: 'CHX32', name: 'Christoph 32', operator: 'ADAC', base: 'Ingolstadt' },
+  { callsign: 'CHX80', name: 'Christoph 80', operator: 'DRF', base: 'Weiden' },
+  { callsign: 'CHX89', name: 'Christoph Regensburg', operator: 'DRF', base: 'Regensburg' },
+
+  // --- Weitere beobachtete Christoph (aus deinem Scan, ohne genaue Base) ---
+  { callsign: 'CHX22', name: 'Christoph 22' },
+  { callsign: 'CHX40', name: 'Christoph 40' },
+  { callsign: 'CHX45', name: 'Christoph 45' },
+  { callsign: 'CHX51', name: 'Christoph 51' },
+  { callsign: 'CHX65', name: 'Christoph 65' },
+  { callsign: 'CHX87', name: 'Christoph 87' },
+
+  // --- Edelweiß (Polizei Bayern) ---
+  { callsign: 'EDL1', name: 'Edelweiß 1', operator: 'Polizei Bayern' },
+  { callsign: 'EDL2', name: 'Edelweiß 2', operator: 'Polizei Bayern' },
+  { callsign: 'EDL3', name: 'Edelweiß 3', operator: 'Polizei Bayern' },
+  { callsign: 'EDL4', name: 'Edelweiß 4', operator: 'Polizei Bayern' },
+  { callsign: 'EDL5', name: 'Edelweiß 5', operator: 'Polizei Bayern' },
+  { callsign: 'EDL6', name: 'Edelweiß 6', operator: 'Polizei Bayern' },
+  { callsign: 'EDL7', name: 'Edelweiß 7', operator: 'Polizei Bayern' },
+  { callsign: 'EDL8', name: 'Edelweiß 8', operator: 'Polizei Bayern' },
+
+  // Varianten mit führender 0 (kommt oft in OpenSky vor)
+  { callsign: 'EDL01', name: 'Edelweiß 1', operator: 'Polizei Bayern' },
+  { callsign: 'EDL02', name: 'Edelweiß 2', operator: 'Polizei Bayern' },
+  { callsign: 'EDL03', name: 'Edelweiß 3', operator: 'Polizei Bayern' },
+  { callsign: 'EDL04', name: 'Edelweiß 4', operator: 'Polizei Bayern' },
+  { callsign: 'EDL05', name: 'Edelweiß 5', operator: 'Polizei Bayern' },
+  { callsign: 'EDL06', name: 'Edelweiß 6', operator: 'Polizei Bayern' },
+  { callsign: 'EDL07', name: 'Edelweiß 7', operator: 'Polizei Bayern' },
+  { callsign: 'EDL08', name: 'Edelweiß 8', operator: 'Polizei Bayern' },
+
+  // --- SAR / Militär ---
+  { callsign: 'SAR1921', name: 'SAR 1921', operator: 'Bundeswehr' },
+  { callsign: 'SAR1975', name: 'SAR 1975', operator: 'Bundeswehr' },
+
+  // --- Sonstige aus deinem Scan ---
+  { callsign: 'POLICER', name: 'Police Helicopter' },
+  { callsign: 'OKPOL', name: 'Police Helicopter (CZ)' },
+  { callsign: 'DEDLG', name: 'Unknown Helicopter' },
+  { callsign: 'VEDLYS1', name: 'Unknown Helicopter' },
+];
